73507e16e59fd9450359e7f444eb2309bdd2a043
RobertAgee <rob.agee@outlook.com>
2022-11-23 13:49:15 -0500

    created node server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const path = require('path');
-const app = express();app.use(express.static(__dirname + '/dist/<app-name>'));
-app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/<app-name>/index.html'));
+const path = require("path");
+const express = require("express");
+const app = express();
+app.use(express.static(__dirname + '/angular-build'));
+app.get('/*', function(req,res){
+res.sendFile(path.join(__dirname, 'angular-build', 'index.html'))
 });
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
 
